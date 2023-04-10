7f1f822100f73d2f8f94940ec8247dfbf282d4b3
AdamLee <ly1130399693@gmail.com>
2023-04-10 16:20:34 +0800

    feat: connect mysql and create user table

---
+++ src/main/resources/db/migration/V20230410153315__create_table_user.sql
@@ -0,0 +1,7 @@
+use `audio`;
+
+CREATE TABLE `user` (
+                        `id` INT(4) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+                        `name` VARCHAR(50) NOT NULL ,
+                        `pwd` VARCHAR(100) NOT NULL
+);
