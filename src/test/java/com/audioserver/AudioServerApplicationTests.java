96da78c9b10157d5c5b4837a39cab56e38b8098c
AdamLee <ly1130399693@gmail.com>
2023-04-10 12:25:06 +0800

    chore: init project by gradle

---
+++ src/test/java/com/audioserver/AudioServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.audioserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class AudioServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertEquals(1,1);
+    }
+
+}
